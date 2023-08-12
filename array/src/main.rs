a86dda91ae126fe5f5a6a2edab6fdfb0eb7eb9fb
cheranratnam87 <cheran.jacob@gmail.com>
2023-08-12 15:41:10 -0500

    small changes

--- array/src/main.rs
+++ array/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
    let mut letters = ['a', 'b', 'c'];
-   letters[0] = 'Y';
+   letters[0] = 'x';
    let first_letter = letters[0];
-   println! ("first_letter issss {}", first_letter);
+   println! ("first_letter is {}", first_letter);
 }
