859adc5d1906bf24ac4013947f336dd4a72051a0
cheranratnam87 <cheran.jacob@gmail.com>
2023-08-12 17:31:00 -0500

    small changes

---
+++ array/src/two_dimensional_array/target/debug/deps/two_dimensional_array.d
@@ -0,0 +1,5 @@
+D:\Rust\Learning\Files\array\src\two_dimensional_array\target\debug\deps\two_dimensional_array.exe: src\main.rs
+
+D:\Rust\Learning\Files\array\src\two_dimensional_array\target\debug\deps\two_dimensional_array.d: src\main.rs
+
+src\main.rs:
---
+++ array/src/two_dimensional_array/target/debug/two_dimensional_array.d
@@ -0,0 +1,1 @@
+D:\Rust\Learning\Files\array\src\two_dimensional_array\target\debug\two_dimensional_array.exe: D:\Rust\Learning\Files\array\src\two_dimensional_array\src\main.rs
